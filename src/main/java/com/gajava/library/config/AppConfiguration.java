31e7a3b44bcd66b95cb596692a1b9cbbfb4ea683
JavaDrinker96 <featureteamleader@gmail.com>
2022-02-12 16:00:28 +0300

    - created config for ModelMapper

---
+++ src/main/java/com/gajava/library/config/AppConfiguration.java
@@ -0,0 +1,25 @@
+package com.gajava.library.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import static org.modelmapper.config.Configuration.AccessLevel.PRIVATE;
+
+@Configuration
+public class AppConfiguration implements WebMvcConfigurer {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.STRICT)
+                .setFieldMatchingEnabled(true)
+                .setSkipNullEnabled(true)
+                .setFieldAccessLevel(PRIVATE);
+        return mapper;
+    }
+
+}
